e5f3bee13d2919f9e88dbf84deab4c7e6bcc6d40
Quill McConnell <quill8197@yahoo.com>
2020-04-17 16:31:07 -0400

    Added header and title

--- index.php
+++ index.php
@@ -9,12 +9,28 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-echo '<h1>PHP Array Practice</h1>';
+echo '<title>Pair Program 2</title>';
+echo '<h1>Pair Program 2</h1>';
+echo '<p>PHP Array Practice</p>';
 
 echo '<p>Step 1</p>';
 $numbers = [7, 9, 8, 9, 8, 8, 6];
 printArr($numbers);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//functions
 function printArr(array $array)
 {
     foreach ($array as $value)
