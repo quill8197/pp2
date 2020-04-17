3e6bdf21e61c1bc0133fb031e851d13d7c62c7ef
Quill McConnell <quill8197@yahoo.com>
2020-04-17 16:27:21 -0400

    Added a function to print array values one line at a time

--- index.php
+++ index.php
@@ -9,4 +9,16 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-echo 'PHP Array Practice';
+echo '<h1>PHP Array Practice</h1>';
+
+echo '<p>Step 1</p>';
+$numbers = [7, 9, 8, 9, 8, 8, 6];
+printArr($numbers);
+
+function printArr(array $array)
+{
+    foreach ($array as $value)
+    {
+        echo '<p>' . $value . '</p>';
+    }
+}
