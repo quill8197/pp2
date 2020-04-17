a9e485d69e2033bee81d7c4b0d0122a985ebc312
Quill McConnell <quill8197@yahoo.com>
2020-04-17 16:17:11 -0400

    Added index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Quill McConnell
+ * 4/17/20
+ * index.php
+ * Pair Program 2
+ */
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo 'PHP Array Practice';
